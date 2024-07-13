928b00b4418e67fdc9a34581dbdef7e2e3afb13e
akashyap25 <anuragkashyap026@gmail.com>
2024-07-13 22:29:33 +0530

    almost working

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    // Change the active indicator on click
+    $('.carousel-indicators-custom .indicator').on('click', function() {
+        var index = $(this).index();
+        $('.carousel-indicators-custom .indicator').removeClass('active');
+        $(this).addClass('active');
+        $('#carousel').carousel(index);
+    });
+
+    
+
+
+
+    // Change the active indicator on slide change
+    $('#carousel').on('slide.bs.carousel', function (e) {
+        var index = $(e.relatedTarget).index();
+        $('.carousel-indicators-custom .indicator').removeClass('active');
+        $('.carousel-indicators-custom .indicator').eq(index).addClass('active');
+    });
+
+    // Change the active indicator on hover
+    $('.hover-card').hover(function() {
+        var index = $(this).closest('.col-md-4').index();
+        $('.carousel-indicators-custom .indicator').removeClass('active');
+        $('.carousel-indicators-custom .indicator').eq(index).addClass('active');
+    });
+});
+
+$(document).ready(function() {
+    $('.project-card').on('click', function() {
+        $('.project-card').removeClass('active');
+
+        $(this).addClass('active');
+
+        var imageUrl = $(this).data('image');
+
+        // Change the src attribute of the project image
+        $('#project-image').attr('src', imageUrl);
+    });
+});
